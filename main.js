ee56869412b5978375edd8b50e4a5c18a4976016
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-12-09 22:01:31 +0100

    init website

---
+++ main.js
@@ -0,0 +1,30 @@
+function loadPage(pageName) {
+    return fetch("pages/"+pageName + '.html')
+        .then(response => response.text())
+        .catch(error => console.error('Error fetching the file:', error));
+}
+
+async function navigate(page) {
+    const contentDiv = document.getElementById('content');
+    let content;
+    switch (page) {
+        case 'home':
+            content = await loadPage('home');
+            break;
+        case 'publication':
+            content = await loadPage('publication');
+            break;
+        case 'project':
+            content = await loadPage('project');
+            break;
+        default:
+            break;
+    }
+    if (content) {
+        contentDiv.innerHTML = content;
+    }
+}
+
+
+// Initially load the home page
+navigate('home');
